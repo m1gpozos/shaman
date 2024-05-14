b92dc7bd76fff32c7d7e1766a1932f934edf710a
m1gpozos <mpozos@gmail.com>
2024-05-14 14:29:30 -0500

    uploaded the correct file this time

--- shaman.rb
+++ shaman.rb
@@ -1,12 +1,15 @@
 require "selenium-webdriver"
 
-driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "http://google.com"
+input_array = ARGV
+
+if input_array[0] == "-i" then
+	prefix = "images"
+else
+	prefix = "www"
+end
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "http://"+prefix+".google.com"
 
 element = driver.find_element(name: 'q')
 element.send_keys "Hello WebDriver!"
 element.submit
-
-puts driver.title
-
-driver.quit
